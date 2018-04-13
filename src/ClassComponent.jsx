0692ef2e07551d27d5b08a17d3c5425662245133
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-13 21:40:02 +0200

    src: moving component into separate files

---
+++ src/ClassComponent.jsx
@@ -0,0 +1,5 @@
+class ClassComponent extends React.Component {
+  render() {
+    return <h3>{this.props.name}</h3>;
+  }
+}
---
+++ src/FunctionalComponent.jsx
@@ -0,0 +1,3 @@
+const FunctionalComponent = (props) => (
+  <h3>{props.name}</h3>
+);
