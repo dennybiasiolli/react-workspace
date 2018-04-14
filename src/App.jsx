996af58d3f97c41c39f7c82e43d94ea7d04a2e1a
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-14 11:15:05 +0200

    src: adding main App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ReactDOM.render((
+  <div>
+    <h1>Hello, React Workspace!</h1>
+    <FunctionalComponent name="Functional Component" />
+    <ClassComponent name="Class Component" />
+    <Clock />
+    <Toggle />
+    <CustomForm />
+    <Calculator />
+  </div>
+), document.getElementById('root'));
