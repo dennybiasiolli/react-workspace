8f38a8bf640e582a939fb062165422b849421277
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-14 19:01:53 +0200

    build: using HtmlWebpackPlugin to inject script into index.html

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src',
@@ -20,5 +21,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   mode: 'development'
 };
