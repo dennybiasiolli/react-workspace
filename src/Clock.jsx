2e4c5b00dd50ca659721efdb769bbd51e400f99e
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-14 09:35:05 +0200

    src: adding lifecycle methods to Clock component

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -4,6 +4,25 @@ class Clock extends React.Component {
     this.state = { date: new Date() };
   }
 
+  componentDidMount() {
+    // setting up the timer when component is rendered to DOM for the first time
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    // clearing the timer when component is removed from the DOM
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
