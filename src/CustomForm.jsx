265aa1b82dfdbaefae5f0cd093fac8f26ef5cab5
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-14 10:29:17 +0200

    src: adding CustomForm component with input handling examples

---
+++ src/CustomForm.jsx
@@ -0,0 +1,88 @@
+class CustomForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.selectable = [
+      { value: 'grapefruit', text: 'Grapefruit' },
+      { value: 'lime', text: 'Lime' },
+      { value: 'coconut', text: 'Coconut' },
+      { value: 'mango', text: 'Mango' }
+    ];
+
+    this.state = {
+      textVal: '',
+      textareaVal: '',
+      selectVal: '',
+      selectMultipleVal: [],
+    };
+  }
+
+  handleChangeText(e) {
+    this.setState({ textVal: e.target.value });
+  }
+
+  handleChangeTextarea(e) {
+    this.setState({ textareaVal: e.target.value });
+  }
+
+  handleChangeSelect(e) {
+    this.setState({ selectVal: e.target.value });
+  }
+
+  handleChangeSelectMultiple(e) {
+    const selectedOptions = [...e.target.options]
+      .filter((o) => o.selected)
+      .map((o) => o.value);
+    this.setState({ selectMultipleVal: selectedOptions });
+  }
+
+  handleSubmit(e) {
+    alert('The form was submitted');
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+        <p>
+          <label>
+            Text:
+            <input type="text" value={this.state.textVal} onChange={(e) => this.handleChangeText(e)} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Textarea:
+            <textarea value={this.state.textareaVal} onChange={(e) => this.handleChangeTextarea(e)} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Pick your favorite La Croix flavor:
+            <select value={this.state.selectVal} onChange={(e) => this.handleChangeSelect(e)}>
+              {
+                this.selectable.map((e) =>
+                  <option key={e.value} value={e.value}>{e.text}</option>
+                )
+              }
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Pick your favorite La Croix flavors:
+            <select multiple={true} value={this.state.selectMultipleVal} onChange={(e) => this.handleChangeSelectMultiple(e)}>
+              {
+                this.selectable.map((e) =>
+                  <option key={e.value} value={e.value}>{e.text}</option>
+                )
+              }
+            </select>
+          </label>
+          {this.state.selectMultipleVal}
+        </p>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
