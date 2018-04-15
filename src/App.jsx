b9ee5b5fbdc11317f2529e88d2ae434d2ff70484
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-15 14:55:40 +0200

    src: using aliased import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import FunctionalComponent from './FunctionalComponent';
-import ClassComponent from './ClassComponent';
-import Clock from './Clock';
-import Toggle from './Toggle';
-import CustomForm from './CustomForm';
-import Calculator from './Calculator';
+import FunctionalComponent from '@/FunctionalComponent';
+import ClassComponent from '@/ClassComponent';
+import Clock from '@/Clock';
+import Toggle from '@/Toggle';
+import CustomForm from '@/CustomForm';
+import Calculator from '@/Calculator';
 
 const App = () => (
   <div>
