e61f5b90f6317bda2fa5ce447c5a69c392f75600
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-15 13:54:33 +0200

    build: second fix of sources for eslint

--- build/webpack.config.dev.babel.js
+++ build/webpack.config.dev.babel.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 import baseConfig from './webpack.config';
 
--- build/webpack.config.prod.babel.js
+++ build/webpack.config.prod.babel.js
@@ -1,6 +1,3 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-
 import baseConfig from './webpack.config';
 
 export default Object.assign({}, baseConfig, {
