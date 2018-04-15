de5db27bc9393aa6563507d6fb157de4f1d9e4d1
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-15 15:20:58 +0200

    test: adding sample snapshot test with react-test-renderer

--- test/unit/Toggle.spec.jsx
+++ test/unit/Toggle.spec.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Toggle from '@/Toggle';
+
 describe('Toggle', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
+  test('should match react-test-renderer snapshot', () => {
+    const component = renderer.create(<Toggle />);
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the toggle
+    tree.children[1].props.onClick();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the toggle
+    tree.children[1].props.onClick();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
