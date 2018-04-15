205580bc51fdb5db9eb550383b9da7598b51d208
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-15 14:18:47 +0200

    src: improving src index and App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import FunctionalComponent from './FunctionalComponent';
 import ClassComponent from './ClassComponent';
@@ -8,16 +7,16 @@ import Toggle from './Toggle';
 import CustomForm from './CustomForm';
 import Calculator from './Calculator';
 
-ReactDOM.render(
-  (
-    <div>
-      <h1>Hello, React Workspace!</h1>
-      <FunctionalComponent name="Functional Component" />
-      <ClassComponent name="Class Component" />
-      <Clock />
-      <Toggle />
-      <CustomForm />
-      <Calculator />
-    </div>
-  ), document.getElementById('root'),
+const App = () => (
+  <div>
+    <h1>Hello, React Workspace!</h1>
+    <FunctionalComponent name="Functional Component" />
+    <ClassComponent name="Class Component" />
+    <Clock />
+    <Toggle />
+    <CustomForm />
+    <Calculator />
+  </div>
 );
+
+export default App;
