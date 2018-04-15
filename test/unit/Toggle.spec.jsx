7ecf751fb16121fd7abf8f9fe3838ee0c9deda8d
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-15 18:02:32 +0200

    test: adding sample snapshot tests with enzyme
    - adding enzyme, enzyme-adapter-react-16 and enzyme-to-json to devDependencies
    
    - configuring jest to setup enzyme-adapter
    
    - adding shallow and mount tests using enzyme-to-json

--- test/unit/Toggle.spec.jsx
+++ test/unit/Toggle.spec.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 import Toggle from '@/Toggle';
 
@@ -21,5 +23,35 @@ describe('Toggle', () => {
     // re-rendering
     tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('should match enzyme shallow snapshot', () => {
+    const wrapper = shallow(<Toggle />);
+    expect(wrapper.find('button')).toHaveLength(3);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    // manually trigger the toggle
+    wrapper.find('button').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    // manually trigger the toggle
+    wrapper.find('button').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('should match enzyme mount snapshot', () => {
+    const wrapper = mount(<Toggle />);
+    expect(wrapper.find('button')).toHaveLength(3);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    // manually trigger the toggle
+    wrapper.find('button').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    // manually trigger the toggle
+    wrapper.find('button').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
