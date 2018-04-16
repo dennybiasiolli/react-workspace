7c45784a501be7944a3a74b053ef67ca3c19652e
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-16 14:24:13 +0200

    src: adding first example store

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Toggle from '@/components/Toggle';
 import CustomForm from '@/components/CustomForm';
 import Calculator from '@/components/Calculator';
 
+import { store } from '@/store';
+
 const App = () => (
   <div>
     <h1>Hello, React Workspace!</h1>
@@ -18,5 +20,16 @@ const App = () => (
     <Calculator />
   </div>
 );
+
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'INCREMENT' });
+// 1
+store.dispatch({ type: 'INCREMENT' });
+// 2
+store.dispatch({ type: 'DECREMENT' });
+// 1
+
 
 export default App;
