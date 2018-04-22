13e3c0cd55c8f99c5944b8da178d4981cdfd537e
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-22 11:52:58 +0200

    src(store): adding todo test containers

---
+++ src/containers/AddTodo.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { addTodo } from '@/store/actions';
+
+const MyAddTodo = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = '';
+        }}
+      >
+        <input
+          ref={(node) => {
+            input = node;
+          }}
+        />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
+};
+
+MyAddTodo.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const AddTodo = connect()(MyAddTodo);
+
+export default AddTodo;
