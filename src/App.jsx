7559832016fec35a12194714498e36451fabd4fc
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2018-04-30 20:21:52 +0200

    src(app): moving each component in a specific route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 import FunctionalComponent from '@/components/FunctionalComponent';
 import ClassComponent from '@/components/ClassComponent';
@@ -20,18 +20,68 @@ const App = () => (
     <BrowserRouter>
       <div>
         <h1>Hello, React Workspace!</h1>
-        <FunctionalComponent name="Functional Component" />
-        <ClassComponent name="Class Component" />
-        <Clock />
-        <CustomForm />
-        <div>
-          <AddTodoComponent />
-          <VisibleTodoList />
-          <Footer />
-        </div>
-        <Toggle />
-        <Calculator />
-        <PostList />
+
+        <Link to="/">Home</Link>
+        &nbsp;|&nbsp;
+        <Link to="/functional-component">Functional Component</Link>
+        &nbsp;|&nbsp;
+        <Link to="/class-component">Class Component</Link>
+        &nbsp;|&nbsp;
+        <Link to="/clock">Clock</Link>
+        &nbsp;|&nbsp;
+        <Link to="/custom-form">CustomForm</Link>
+        &nbsp;|&nbsp;
+        <Link to="/todo">Todo</Link>
+        &nbsp;|&nbsp;
+        <Link to="/toggle">Toggle</Link>
+        &nbsp;|&nbsp;
+        <Link to="/calculator">Calculator</Link>
+        &nbsp;|&nbsp;
+        <Link to="/post-list">PostList</Link>
+
+        <hr />
+
+        <Switch>
+          <Route
+            exact
+            path="/functional-component"
+            render={props =>
+              <FunctionalComponent {...props} name="Functional Component" />}
+          />
+
+          <Route
+            exact
+            path="/class-component"
+            render={props =>
+              <ClassComponent {...props} name="Class Component" />}
+          />
+
+
+          <Route exact path="/clock" component={Clock} />
+
+          <Route exact path="/custom-form" component={CustomForm} />
+
+          <Route exact path="/todo">
+            <div>
+              <AddTodoComponent />
+              <VisibleTodoList />
+              <Footer />
+            </div>
+          </Route>
+
+          <Route exact path="/toggle" component={Toggle} />
+
+          <Route exact path="/calculator" component={Calculator} />
+
+          <Route exact path="/post-list" component={PostList} />
+
+          {/* when none of the above match, the following route will be rendered */}
+          <Route>
+            <h3>Select a demo component</h3>
+          </Route>
+
+
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
